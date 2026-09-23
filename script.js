c23bd4730985d45ac432a66c04ee14ee411fc86c
jasonac <143886081+jasonac1@users.noreply.github.com>
2026-09-23 11:54:08 +0700

    Add basic library logic

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Test");
+const myLibrary = [];
+
+function Book(title, author, publisher, date) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.publisher = publisher;
+    this.date = date;
+}
+
+function addBookToLibrary(title, author, publisher, date) {
+    let newBook = new Book(title, author, publisher, date);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("Pride and Prejudice", "Jane Austen", "Self", 1813);
+addBookToLibrary("Moby Dick", "Herman Melville", "Self", 1851);
+addBookToLibrary("War and Peace", "Leo Tolstoy", "Self", 1867);
+
+for(let book of myLibrary) {
+    console.log(book.title + " - " + book.author);
+}
